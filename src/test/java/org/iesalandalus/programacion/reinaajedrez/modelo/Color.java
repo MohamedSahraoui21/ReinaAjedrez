13ef63ce3f1d19cbdc4a87d59af1857fc5cd5a97
SIMO <SIMO@DESKTOP-QGHA210>
2022-11-17 14:32:56 +0100

    he creado los constructores de la reina

--- src/test/java/org/iesalandalus/programacion/reinaajedrez/modelo/Color.java
+++ src/test/java/org/iesalandalus/programacion/reinaajedrez/modelo/Color.java
@@ -1,5 +1,16 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
 public enum Color {
+	BLANCO("Blanco"),NEGRO("Negro");
+	String CadenaMostrar;
 
+	private Color(String cadenaMostrar) {
+		this.CadenaMostrar = cadenaMostrar;
+	}
+	
+   @Override
+   public String toString() {
+	   return String.format(CadenaMostrar);
+   }
+   
 }
--- src/test/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/test/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -1,5 +1,34 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
 public class Reina {
+	private Posicion posicion;
+	private Color color;
+public Reina() {
+	posicion= new Posicion(1, 'd');
+	color=color.BLANCO;
+}
+
+public Reina(Color color) {
+	if (color==color.BLANCO) {
+		posicion= new Posicion(1, 'd');
+	}
+	if (color==color.NEGRO) {
+		posicion= new Posicion(8, 'd');
+	}
+    
+}
+public Posicion getPosicion() {
+	return posicion;
+}
+public Color getColor() {
+	return color;
+}
+private void setPosicion(Posicion posicion) {
+	this.posicion = posicion;
+}
+private void setColor(Color color) {
+	this.color = color;
+}
+ 
 
 }
