aa9ba13b58a127f238c08867628405b6fc62c21c
SIMO <SIMO@DESKTOP-QGHA210>
2022-11-18 11:26:42 +0100

    he modificado las clases posicion y reina

--- src/test/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
+++ src/test/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -3,64 +3,74 @@ package org.iesalandalus.programacion.reinaajedrez.modelo;
 import java.util.Objects;
 
 public class Posicion {
-    private int fila;
-    private char columna;
-public Posicion(int fila,char columna){
-	 setFila(fila);
-	 setColumna(columna);
-}
-	 
+	private int fila;
+	private char columna;
 
-public Posicion(Posicion posicion) {
-	 if (posicion==null) {
-		 throw new NullPointerException("la posicion no puede ser nula");
-	 }
-}
-private void setFila(int fila) {
-	
-	if (fila<1 || fila>8) {
-		 throw new NullPointerException("la fila no puede ser menos de 1 y mas de 8");
-}
-	this.fila = fila;
-}
-private void setColumna(char columna) {
-	if (columna<'a' || columna>'h') {
-		 throw new NullPointerException("la fila tiene que ser entre a y h ambos incluidos");
-		
-}
-	this.columna = columna;
-}
-public int getFila() {
-	return fila;
-}
-public char getColumna() {
-	return columna;
-}
+	public Posicion(int fila, char columna) {
+		if (fila < 1 || fila > 8) {
+			throw new IllegalArgumentException("ERROR: Fila no válida.");
+		}
+		this.fila = fila;
 
+		if (columna < 'a' || columna > 'h') {
+			throw new IllegalArgumentException("ERROR: Columna no válida.");
 
-@Override
-public int hashCode() {
-	return Objects.hash(columna, fila);
-}
+		}
+		this.columna = columna;
+	}
 
+	public Posicion(Posicion posicion) {
+		if (posicion == null) {
+			throw new NullPointerException("ERROR: No es posible copiar una posición nula.");
+		}
+		this.fila=posicion.getFila();
+		this.columna=posicion.getColumna();
+	}
 
-@Override
-public boolean equals(Object obj) {
-	if (this == obj)
-		return true;
-	if (obj == null)
-		return false;
-	if (getClass() != obj.getClass())
-		return false;
-	Posicion other = (Posicion) obj;
-	return columna == other.columna && fila == other.fila;
-}
+	private void setFila(int fila) throws IllegalAccessException {
 
+		if (fila < 1 || fila > 8) {
+			throw new IllegalAccessException("ERROR: Fila no válida.");
+		}
+		this.fila = fila;
+	}
 
-@Override
-public String toString() {
-	return "fila=" + fila + ", columna=" + columna;
-}
- 
+	private void setColumna(char columna) throws IllegalAccessException {
+		if (columna < 'a' || columna > 'h') {
+			throw new IllegalAccessException("ERROR: Fila no válida.");
+
+		}
+		this.columna = columna;
+	}
+
+	public int getFila() {
+		return fila;
+	}
+
+	public char getColumna() {
+		return columna;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(columna, fila);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Posicion other = (Posicion) obj;
+		return columna == other.columna && fila == other.fila;
+	}
+
+	@Override
+	public String toString() {
+		return "fila=" + fila + ", columna=" + columna;
+	}
 
 }
--- src/test/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/test/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -5,113 +5,123 @@ import javax.naming.OperationNotSupportedException;
 public class Reina {
 	private Posicion posicion;
 	private Color color;
-public Reina() {
-	posicion= new Posicion(1, 'd');
-	color=Color.BLANCO;
-}
 
-public Reina(Color color) {
-	if (color==Color.BLANCO) {
-		posicion= new Posicion(1, 'd');
+	public Reina() {
+		posicion = new Posicion(1, 'd');
+		color = Color.BLANCO;
 	}
-	if (color==Color.NEGRO) {
-		posicion= new Posicion(8, 'd');
+
+	public Reina(Color color) {
+		if(color==null) {
+			throw new NullPointerException("ERROR: El color no puede ser nulo.");
+		}
+		if (color == Color.BLANCO) {
+			posicion = new Posicion(1, 'd');
+		}
+		if (color == Color.NEGRO) {
+			posicion = new Posicion(8, 'd');
+		}
+		this.color=color;
+		
 	}
-    
-}
-public Posicion getPosicion() {
-	return posicion;
-}
-public Color getColor() {
-	return color;
-}
-private void setPosicion(Posicion posicion) {
-	this.posicion = posicion;
-}
-private void setColor(Color color) {
-	this.color = color;
-}
-public void mover(Direccion direccion, int pasos) throws OperationNotSupportedException {
-if (direccion == null) {
-throw new NullPointerException("ERROR: La dirección no puede ser nula.");
-}
-if (pasos < 1 || pasos > 7) {
-throw new IllegalArgumentException("ERROR: El número de pasos debe estar comprendido entre 1 y 7.");
-}
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	private void setPosicion(Posicion posicion) {
+		this.posicion = posicion;
+	}
+
+	private void setColor(Color color) {
+		this.color = color;
+	}
+
+	public void mover(Direccion direccion, int pasos) throws OperationNotSupportedException {
+		if (direccion == null) {
+			throw new NullPointerException("ERROR: La dirección no puede ser nula.");
+		}
+		if (pasos < 1 || pasos > 7) {
+			throw new IllegalArgumentException("ERROR: El número de pasos debe estar comprendido entre 1 y 7.");
+		}
 /////////////////////////////////////////////////
-System.out.println("-- Vamos a mover la reina -- ");
-switch (direccion) {
-case NORTE:
-if (posicion.getFila() + pasos > 8) {
-posicion = getPosicion();
-throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
-} else
-setPosicion(new Posicion(posicion.getFila() + pasos, posicion.getColumna()));
-break;
+		
+		switch (direccion) {
+		case NORTE:
+			if (posicion.getFila() + pasos > 8) {
+				posicion = getPosicion();
+				throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
+			} else
+				setPosicion(new Posicion(posicion.getFila() + pasos, posicion.getColumna()));
+			break;
 
-case SUR:
-if (posicion.getFila() - pasos <= 0) {
-posicion = getPosicion();
-throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
-} else
-setPosicion(new Posicion(posicion.getFila() - pasos, posicion.getColumna()));
-break;
+		case SUR:
+			if (posicion.getFila() - pasos <= 0) {
+				posicion = getPosicion();
+				throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
+			} else
+				setPosicion(new Posicion(posicion.getFila() - pasos, posicion.getColumna()));
+			break;
 
-case ESTE:
-if (posicion.getColumna() + (char) pasos > 'h') {
-posicion = getPosicion();
-throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
-} else
-setPosicion(new Posicion(posicion.getFila(), (char) (posicion.getColumna() + pasos)));
-break;
+		case ESTE:
+			if (posicion.getColumna() + (char) pasos > 'h') {
+				posicion = getPosicion();
+				throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
+			} else
+				setPosicion(new Posicion(posicion.getFila(), (char) (posicion.getColumna() + pasos)));
+			break;
 
-case OESTE:
-if (posicion.getColumna() - (char) pasos < 'a') {
-posicion = getPosicion();
-throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
-} else
-setPosicion(new Posicion(posicion.getFila(), (char) (posicion.getColumna() - pasos)));
-break;
+		case OESTE:
+			if (posicion.getColumna() - (char) pasos < 'a') {
+				posicion = getPosicion();
+				throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
+			} else
+				setPosicion(new Posicion(posicion.getFila(), (char) (posicion.getColumna() - pasos)));
+			break;
 
-case NORESTE:
-if (posicion.getFila() + pasos > 8 || posicion.getColumna() + (char) pasos > 'h') {
-posicion = getPosicion();
-throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
-} else
-setPosicion(new Posicion(posicion.getFila() + pasos, (char) (posicion.getColumna() + pasos)));
-break;
+		case NORESTE:
+			if (posicion.getFila() + pasos > 8 || posicion.getColumna() + (char) pasos > 'h') {
+				posicion = getPosicion();
+				throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
+			} else
+				setPosicion(new Posicion(posicion.getFila() + pasos, (char) (posicion.getColumna() + pasos)));
+			break;
 
-case SUROESTE:
-if (posicion.getFila() - pasos <= 0 || posicion.getColumna() - (char) pasos < 'a') {
-posicion = getPosicion();
-throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
-} else
-setPosicion(new Posicion(posicion.getFila() - pasos, (char) (posicion.getColumna() - pasos)));
-break;
+		case SUROESTE:
+			if (posicion.getFila() - pasos <= 0 || posicion.getColumna() - (char) pasos < 'a') {
+				posicion = getPosicion();
+				throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
+			} else
+				setPosicion(new Posicion(posicion.getFila() - pasos, (char) (posicion.getColumna() - pasos)));
+			break;
 
-case SURESTE:
-if (posicion.getFila() - pasos <= 0 || posicion.getColumna() + (char) pasos > 'h') {
-posicion = getPosicion();
-throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
-} else
-setPosicion(new Posicion(posicion.getFila() - pasos, (char) (posicion.getColumna() + pasos)));
-break;
+		case SURESTE:
+			if (posicion.getFila() - pasos <= 0 || posicion.getColumna() + (char) pasos > 'h') {
+				posicion = getPosicion();
+				throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
+			} else
+				setPosicion(new Posicion(posicion.getFila() - pasos, (char) (posicion.getColumna() + pasos)));
+			break;
 
-case NOROESTE:
-if (posicion.getFila() + pasos > 8 || posicion.getColumna() - (char) pasos < 'a') {
-posicion = getPosicion();
-throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
-} else
-setPosicion(new Posicion(posicion.getFila() + pasos, (char) (posicion.getColumna() - pasos)));
-break;
-default:
-break;
-}
-}
+		case NOROESTE:
+			if (posicion.getFila() + pasos > 8 || posicion.getColumna() - (char) pasos < 'a') {
+				posicion = getPosicion();
+				throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
+			} else
+				setPosicion(new Posicion(posicion.getFila() + pasos, (char) (posicion.getColumna() - pasos)));
+			break;
+		default:
+			break;
+		}
+	}
 
-
-
-
- 
+	@Override
+	public String toString() {
+		return String.format("color=%s, posicion=(%s)", color, posicion);
+	}
 
 }
